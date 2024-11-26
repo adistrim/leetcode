69ae9cf1f5f855f24bec994c1ae9892a51953bc5
Aditya <98259698+adistrim@users.noreply.github.com>
2024-11-26 19:07:22 +0530

    Time: 18 ms (22.19%), Space: 17.9 MB (10.91%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+
+        # approach would be to 
+        # ''.join(sorted(var_name)) that's how we sort a string [properly]
+
         s = ''.join(sorted(s))
         t = ''.join(sorted(t))
 
