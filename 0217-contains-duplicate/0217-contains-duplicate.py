60cdfc0f73967b932b14b3b2506f4302fdc1a46c
Aditya <98259698+adistrim@users.noreply.github.com>
2024-11-26 18:53:25 +0530

    Time: 8 ms (68.41%), Space: 31.2 MB (50.49%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,8 +1,16 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        sseett = set()
+
+        # create a hashmap (called set in python)
+        # iterate through the elements of the nums array
+        # check if the current element is present in the hashmap or not
+        # if not present then add that element into the hashmap
+        # if present then return True because that indicates duplicate
+
+        hashmap = set()
         for i in nums:
-            if i in sseett:
+            if i in hashmap:
                 return True
-            sseett.add(i)
+            hashmap.add(i)
         return False
+
